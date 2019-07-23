46098562103f1136809f15811c7afb9348fd9bf7
Rahul Nonia <rahul.kr.nonia@gmail.com>
2019-07-23 10:42:01 +0530

    BFS using adjacency list

---
+++ BFS/implementation/adjMatrix.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : adjMatrix.cpp
+ * Created on    : Tuesday 23 July 2019 09:06:46 AM IST
+ * Last modified : Tuesday 23 July 2019 09:32:37 AM IST
+ * Description   : implementation of breadth first search using adjacency
+ *                 matrix. 
+ * ***********************************************************************/
+
+
+#include <iostream>
+
+
+int main ()
+{
+	int v = 5;
+	bool graph[v][v] = {
+					 {0, 1, 0, 1, 0},
+					 {},
+					 {},
+					 {},
+					 {},
+					 };
+
+	return 0;
+}
---
+++ BFS/questions/level.cpp
@@ -0,0 +1,15 @@
+/* ************************************************************************
+ * Created by    : Rahul Kumar Nonia
+ * File name     : level.cpp
+ * Created on    : Tuesday 23 July 2019 10:40:21 AM IST
+ * Last modified : Tuesday 23 July 2019 10:41:02 AM IST
+ * Description   : Determining the level of each node using BFS
+ * ***********************************************************************/
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
